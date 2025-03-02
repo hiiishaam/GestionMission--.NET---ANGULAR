e5506aad9998163f9a7a190ac7b7aed9c71c65aa
hicham <hichamwh1996@gmail.com>
2025-03-02 18:20:41 +0100

    Initial commit

---
+++ GestionMission/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GestionMission.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionMission.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employer> employers { get; set; }
+    }
+}
---
+++ GestionMission/Entities/Employer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace GestionMission.Entities
+{
+    public class Employer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Nom { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Prenom { get; set; }
+    }
+}
